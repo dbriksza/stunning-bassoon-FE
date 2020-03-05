b52e89f425ba9fcefd5d4937ba19265c3d83632f
hyetigran <tigran.asriyan@gmail.com>
2020-03-05 14:27:37 -0500

    added routes to app component

--- game_rpg/src/App.js
+++ game_rpg/src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
+
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <AppWrapper>
+      <Route
+        exact
+        path="/register"
+        render={props => {
+          <Register {...props} />;
+        }}
+      />
+      <Route
+        exact
+        path="/"
+        render={props => {
+          <Login {...props} />;
+        }}
+      />
+    </AppWrapper>
   );
 }
+
+const AppWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  width: 100%;
+`;
 
 export default App;
---
+++ game_rpg/src/components/Auth/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = props => {
+  return (
+    <div>
+      <div> login form</div>
+    </div>
+  );
+};
+
+export default Login;
