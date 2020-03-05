1872be8642ee2d41522a3f4797554665654282f9
gelinas <jlgelinas@gmail.com>
2020-03-05 11:57:36 -0500

    bug fix: render error

--- game_rpg/src/index.js
+++ game_rpg/src/index.js
@@ -7,6 +7,7 @@ import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import rootReducer from "./store/reducers"
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
