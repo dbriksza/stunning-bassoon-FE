5e2d0f39dfb104a0995d47c2160bd202e08cb43e
gelinas <jlgelinas@gmail.com>
2020-03-05 17:20:16 -0500

    feat: add pusher

--- game_rpg/src/App.js
+++ game_rpg/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
+
+// pusher
+import Pusher from 'pusher-js';
+
+// app
 import Game from "./components/gameboard/Game";
 import "./App.scss";
+
+const pusher = new Pusher('0805c1e228898d83231c', {
+  cluster: 'us2',
+  forceTLS: true
+});
+
+const channel = pusher.subscribe('game-channel');
+
+channel.bind('update-world', function(data) {
+  alert(JSON.stringify(data));
+});
 
 function App() {
   return (
--- game_rpg/src/index.js
+++ game_rpg/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+// redux
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
-import rootReducer from "./store/reducers"
+import rootReducer from "./store/reducers";
+
+// app
+import "./index.css";
+import App from "./App";
+
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
