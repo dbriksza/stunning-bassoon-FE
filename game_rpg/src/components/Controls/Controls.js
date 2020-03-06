7f823419ea7c0db3ee78a11b13cb58e3fa7f70b3
hyetigran <tigran.asriyan@gmail.com>
2020-03-06 11:52:21 -0500

    control component basics

--- game_rpg/src/components/Controls/Controls.js
+++ game_rpg/src/components/Controls/Controls.js
@@ -1,1 +1,13 @@
 // control for sending player movment roll 'POST' requests and move 'POST' requests
+
+import React from "react";
+
+const Controls = props => {
+  return (
+    <div>
+      <div>Controls</div>
+    </div>
+  );
+};
+
+export default Controls;
